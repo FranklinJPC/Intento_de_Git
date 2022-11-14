8137929df9cc182facc7f81cd8739bb4197e83fc
Franklin <franklin.patino@epn.edu.ec>
2022-11-14 10:56:37 -0500

    Setter and Getter

--- src/Universidades.java
+++ src/Universidades.java
@@ -3,6 +3,59 @@ public class Universidades
     float notas,promedios;
     String codigo_unico,nombre,apéllido,cedula,domicilio;
 
+    public float getNotas() {
+        return notas;
+    }
 
+    public void setNotas(float notas) {
+        this.notas = notas;
+    }
 
+    public float getPromedios() {
+        return promedios;
+    }
+
+    public void setPromedios(float promedios) {
+        this.promedios = promedios;
+    }
+
+    public String getCodigo_unico() {
+        return codigo_unico;
+    }
+
+    public void setCodigo_unico(String codigo_unico) {
+        this.codigo_unico = codigo_unico;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApéllido() {
+        return apéllido;
+    }
+
+    public void setApéllido(String apéllido) {
+        this.apéllido = apéllido;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
 }
